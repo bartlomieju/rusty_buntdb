42e8912709d6384aa5cd5a233a2fa1a06b80b192
Bartek Iwańczuk <biwanczuk@gmail.com>
2021-07-22 21:52:22 +0200

    port ascend/descend helpers

--- src/lib.rs
+++ src/lib.rs
@@ -1659,33 +1659,37 @@ impl<'db> Tx<'db> {
 
         if desc {
             if gt {
-                todo!()
+                if lt {
+                    btree_descend_range(
+                        tr,
+                        item_a.as_ref().unwrap(),
+                        item_b.as_ref().unwrap(),
+                        iterator,
+                    );
+                } else {
+                    btree_descend_greater_than(tr, item_a.as_ref().unwrap(), iterator);
+                }
             } else if lt {
-                tr.descend(item_a, |item| iterator(&item.key, &item.val));
+                btree_descend_less_or_equal(tr, item_a, iterator);
             } else {
-                tr.descend(None, |item| {
-                    let val = iterator(&item.key, &item.val);
-                    eprintln!("tr descend {:#?} {}", item.key, val);
-                    val
-                });
+                btree_descend(tr, iterator);
             }
         } else {
             if gt {
                 if lt {
-                    // tr.ascend(item_a.clone(), |item| {
-                    //     btree_lt(tr, item_a.as_ref().unwrap(), item) && iterator(&item.key, &item.val)
-                    // });
+                    btree_ascend_range(
+                        tr,
+                        item_a.as_ref().unwrap(),
+                        item_b.as_ref().unwrap(),
+                        iterator,
+                    );
                 } else {
-                    tr.ascend(item_a, |item| iterator(&item.key, &item.val));
+                    btree_ascend_greater_or_equal(tr, item_a, iterator);
                 }
             } else if lt {
-                todo!()
+                btree_ascend_less_than(tr, item_a.as_ref().unwrap(), iterator);
             } else {
-                tr.ascend(None, |item| {
-                    let val = iterator(&item.key, &item.val);
-                    eprintln!("tr ascend {:#?} {}", item.key, val);
-                    val
-                });
+                btree_ascend(tr, iterator);
             }
         }
 
@@ -1978,6 +1982,82 @@ fn btree_lt(tree: &BTreeC<DbItem>, a: &DbItem, b: &DbItem) -> bool {
 
 fn btree_gt(tree: &BTreeC<DbItem>, a: &DbItem, b: &DbItem) -> bool {
     tree.less(b, a)
+}
+
+// Ascend helpers
+
+fn btree_ascend<F>(tree: &BTreeC<DbItem>, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.ascend(None, |item| iterator(&item.key, &item.val));
+}
+
+fn btree_ascend_less_than<F>(tree: &BTreeC<DbItem>, pivot: &DbItem, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.ascend(None, |item| {
+        btree_lt(tree, item, pivot) && iterator(&item.key, &item.val)
+    });
+}
+
+fn btree_ascend_greater_or_equal<F>(tree: &BTreeC<DbItem>, pivot: Option<DbItem>, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.ascend(pivot, |item| iterator(&item.key, &item.val));
+}
+
+fn btree_ascend_range<F>(
+    tree: &BTreeC<DbItem>,
+    greater_or_equal: &DbItem,
+    less_than: &DbItem,
+    mut iterator: F,
+) where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.ascend(Some(greater_or_equal.clone()), |item| {
+        btree_lt(tree, item, less_than) && iterator(&item.key, &item.val)
+    });
+}
+
+// Descend helpers
+
+fn btree_descend<F>(tree: &BTreeC<DbItem>, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.descend(None, |item| iterator(&item.key, &item.val));
+}
+
+fn btree_descend_greater_than<F>(tree: &BTreeC<DbItem>, pivot: &DbItem, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.descend(None, |item| {
+        btree_gt(tree, item, pivot) && iterator(&item.key, &item.val)
+    });
+}
+
+fn btree_descend_range<F>(
+    tree: &BTreeC<DbItem>,
+    less_or_equal: &DbItem,
+    greater_than: &DbItem,
+    mut iterator: F,
+) where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.descend(Some(less_or_equal.clone()), |item| {
+        btree_gt(tree, item, greater_than) && iterator(&item.key, &item.val)
+    });
+}
+
+fn btree_descend_less_or_equal<F>(tree: &BTreeC<DbItem>, pivot: Option<DbItem>, mut iterator: F)
+where
+    F: FnMut(&str, &str) -> bool,
+{
+    tree.descend(pivot, |item| iterator(&item.key, &item.val));
 }
 
 // index_int is a helper function that returns true if 'a` is less than 'b'
